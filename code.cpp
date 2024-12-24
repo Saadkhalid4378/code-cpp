98ed38a97bdd61c2134d741b2040a11ea91d508f
Saad khalid <saadali4378@gmail.com>
2024-12-24 16:19:19 +0500

    first commit

---
+++ code.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std:
+int n , a;
+cin >> n >> "user input";
+
+int main {
+    if (n == n%1) 
+    {
+    a = n^3;
+    z = a + n;
+    cout << z; }
+    else {
+    cout << "not a prime number" ;
+    }
+
+}
+
+
+5  , 9
+
+
+13579
+
+
+int a , b
+cout << "enter a " 
+cin << "enter a " a
+cout << "enter b " b
+cin << "enter b " b
+
+
+int main {
+    if 
+}
+
+
